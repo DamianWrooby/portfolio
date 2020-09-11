040d21e7c8291ba3a733a568c5093c93253403c7
Damian Wróblewski <dwroblewski89@gmail.com>
2020-09-11 21:51:39 +0200

    Setup test environment properly

--- src/__tests__/Links.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Links from "../components/organisms/Navigation/Links";
-
-test("Render Links component", () => {
-  const { getByTestId } = render(<Links />);
-  expect(getByTestId("Home")).toBeInTheDocument();
-});
