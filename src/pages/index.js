e722ad1360299c08332f9a23375bf3c61b6f1fbc
Damian Wróblewski <dwroblewski89@gmail.com>
2020-10-06 21:01:39 +0200

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Layout from "../layouts/Layout";
+import Layout from "../layouts/layout";
 import Header from "../components/organisms/Header/Header";
 import Navigation from "../components/organisms/Navigation/Navigation";
 import Technologies from "../components/organisms/Technologies/Technologies";
 import Projects from "../components/organisms/Projects/Projects";
 import Contact from "../components/organisms/Contact/Contact";
+import Footer from "../components/molecules/Footer/Footer";
 import NavigationProvider from "../contexts/NavigationContext";
 
 const IndexPage = () => (
@@ -17,7 +18,7 @@ const IndexPage = () => (
         <Projects />
         <Contact />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </Layout>
   </NavigationProvider>
 );
