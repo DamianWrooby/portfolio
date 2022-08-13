67f2d8cc85f1cf077c2c743a2e182763a7a8f28d
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2022-08-13 13:29:25 +0200

    update technologies list and improve icons importing

--- src/components/molecules/TechnologiesList/TechnologiesList.js
+++ src/components/molecules/TechnologiesList/TechnologiesList.js
@@ -2,16 +2,7 @@ import gsap from "gsap";
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-import firebaseIcon from "../../../assets/icons/firebase.svg";
-import gatsbyIcon from "../../../assets/icons/gatsby.svg";
-import gsapIcon from "../../../assets/icons/gsap.svg";
-import jestIcon from "../../../assets/icons/jest.svg";
-import nextIcon from "../../../assets/icons/next.svg";
-import nuxtIcon from "../../../assets/icons/nuxt.svg";
-import prismaIcon from "../../../assets/icons/prisma.svg";
-import reactIcon from "../../../assets/icons/react.svg";
-import vueIcon from "../../../assets/icons/vue.svg";
-import wpIcon from "../../../assets/icons/wp.svg";
+import icons from "../../../utils/icons";
 import ListItem from "../../atoms/ListItem/ListItem";
 
 const ListsWrapper = styled.div`
@@ -30,9 +21,9 @@ const ListsWrapper = styled.div`
 `;
 
 const List = styled.ul`
-  display: flex;
-  flex-direction: column;
-  padding: 0 50px 0 0px;
+	display: flex;
+	flex-direction: column;
+	padding: 0 50px 0 0px;
 `;
 
 const ItemList = () => {
@@ -60,26 +51,46 @@ const ItemList = () => {
 	return (
 		<ListsWrapper>
 			<List ref={list1Ref}>
-				<ListItem icon={reactIcon} height="25">React</ListItem>
-				<ListItem icon={nextIcon}>Next.js</ListItem>
-				<ListItem icon={gatsbyIcon}>Gatsby</ListItem>
-				<ListItem icon={vueIcon} height="25" width="25">Vue + Vuex</ListItem>
-				<ListItem icon={nuxtIcon} height="25" width="25">Nuxt</ListItem>
+				<ListItem icon={icons.react} height="25">
+					React
+				</ListItem>
+				<ListItem icon={icons.next}>Next.js</ListItem>
+				<ListItem icon={icons.gatsby}>Gatsby</ListItem>
+				<ListItem icon={icons.redux} height="25" width="25">
+					Redux
+				</ListItem>
+				<ListItem icon={icons.vue} height="25" width="25">
+					Vue + Vuex
+				</ListItem>
+				<ListItem icon={icons.nuxt} height="25" width="25">
+					Nuxt
+				</ListItem>
+				<ListItem icon={icons.angular} height="25" width="25">
+					Angular
+				</ListItem>
 			</List>
 			<List ref={list2Ref}>
-				<ListItem icon={jestIcon} height="23">
+				<ListItem icon={icons.ts} height="23">
+					TypeScript
+				</ListItem>
+				<ListItem icon={icons.docker} height="23">
+					Docker
+				</ListItem>
+				<ListItem icon={icons.jest} height="23">
 					Jest
 				</ListItem>
-				<ListItem icon={gsapIcon} height="23">
+				<ListItem icon={icons.gsap} height="23">
 					GSAP
 				</ListItem>
-				<ListItem icon={firebaseIcon} height="28">
+				<ListItem icon={icons.firebase} height="28">
 					Firebase
 				</ListItem>
-				<ListItem icon={prismaIcon} height="25" width="25">
+				<ListItem icon={icons.prisma} height="25" width="25">
 					Prisma
 				</ListItem>
-				<ListItem icon={wpIcon}>WordPress</ListItem>
+				<ListItem icon={icons.contentful} height="23">
+					Contentful
+				</ListItem>
 			</List>
 		</ListsWrapper>
 	);
---
+++ src/utils/icons.js
@@ -0,0 +1,33 @@
+import angular from "../assets/icons/angular.svg";
+import contentful from "../assets/icons/contentful.svg";
+import docker from "../assets/icons/docker.svg";
+import firebase from "../assets/icons/firebase.svg";
+import gatsby from "../assets/icons/gatsby.svg";
+import gsap from "../assets/icons/gsap.svg";
+import jest from "../assets/icons/jest.svg";
+import next from "../assets/icons/next.svg";
+import nuxt from "../assets/icons/nuxt.svg";
+import prisma from "../assets/icons/prisma.svg";
+import react from "../assets/icons/react.svg";
+import redux from "../assets/icons/redux.svg";
+import ts from "../assets/icons/ts.svg";
+import vue from "../assets/icons/vue.svg";
+
+const icons = {
+	angular,
+	contentful,
+	firebase,
+	gatsby,
+	gsap,
+	jest,
+	next,
+	nuxt,
+	prisma,
+	react,
+	redux,
+	vue,
+	ts,
+	docker,
+};
+
+export default icons;
